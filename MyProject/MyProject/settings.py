316074682a0aaea8e05afc5b673b3f0118325d68
SheetalThakur2003 <122449394+SheetalThakur2003@users.noreply.github.com>
2024-09-27 16:56:28 +0530

    Update settings.py

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -80,6 +80,8 @@ DATABASES = {
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': 'mongodb+srv://Anju123:Anju%40%2302@cluster0.fybbg.mongodb.net/',  # Replace with your MongoDB Atlas cluster hostname
+            'ssl': True,
+            'ssl_cert_reqs': ssl.CERT_NONE,  # Add this to disable cert verification
 
 
         }
